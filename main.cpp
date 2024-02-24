87dfdaeb49b725ff7c7b246dd4fe94a797f4be05
HungMingWu <u9089000@gmail.com>
2024-02-24 16:34:39 +0800

    Implement a sender

--- main.cpp
+++ main.cpp
@@ -1,7 +1,31 @@
 import mod;
+#include <iostream>
 #include <stdexec/execution.hpp>
+#include "exec/static_thread_pool.hpp"
+
+using namespace stdexec;
+
+sender auto create(sender auto begin)
+{
+    sender auto first = then(
+        begin,
+        [&](int v) {
+            return v + 1;
+        });
+    sender auto second = then(
+        first,
+        [](int v) {
+            return v * 100;
+        });
+    return second;
+}
 
 int main()
 {
-	return 0;
+    exec::static_thread_pool ctx{ 8 };
+    scheduler auto sch = ctx.get_scheduler();
+    auto start = then(schedule(sch), []() { return 999; });
+    auto [value] = sync_wait(create(start)).value();
+    std::cout << value << "\n";
+    return 0;
 }
